0467f3d17a77d296e0be49a5f675e1d86873b4ec
serverless <vasusahu4008@gmail.com>
2026-08-30 18:22:13 +0530

    fetching questions, filtering and deduping

--- server/scripts/fetchCF.js
+++ server/scripts/fetchCF.js
@@ -1,27 +1,33 @@
 async function main(){
     const response1 = await fetch("https://codeforces.com/api/user.status?handle=Vasu.609");    
-    const response2 = await fetch("https://codeforces.com/api/user.info?handles=Vasu.609");
-    
+    // const response2 = await fetch("https://codeforces.com/api/user.info?handles=Vasu.609");
     const userSubmission = await response1.json();
     // const userProfile = await response2.json();
 
     //todo: implement filter to result to only verdict === 'OK'
-    let filteredSubmission = userSubmission.result.filter(s => s.verdict === 'OK');
+    const requiredSubmission = userSubmission.result.filter(s => s.verdict === 'OK');
 
     //todo: dedupe by problem.contestID + '-' + prolem.index (use set or map)
-    const uniqueMap = new Map(
-        filteredSubmission.map(s => [`${s.problem.contestId} - ${s.problem.index}`, s])
+    const uniqueResultMap = new Map(
+        requiredSubmission.map(s => [`${s.problem.contestId} - ${s.problem.index}`, s])
     )
+    
 
     //todo: log the count of unique solved problem and log just first 3 deduped problem (with rating and tags)
-    const uniqueSubmission = [...uniqueMap.values()];
+    const uniqueSubmissions = [...uniqueResultMap.values()];
 
-    console.log('Unique solved count:', uniqueSubmission.length);
-    console.log(uniqueSubmission.slice(0, 4).map(s =>({
+    console.log('Unique solved count:', uniqueSubmissions.length);
+    console.log(uniqueSubmissions.slice(0, 3).map(s =>({
         name: s.problem.name,
         rating: s.problem.rating,
+        id: s.problem.contestId,
+        index: s.problem.index,
         tags: s.problem.tags
     })))
+
+    // fetch -> filter ok -> deduplicate -> count total number of problem solved -> log result
+
 }
 
 main();
+
